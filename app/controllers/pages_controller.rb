23bc9dfe39fe69f12957919d31a5f457e1a79ca6
Connor Finnerty <connorfinnerty@gmail.com>
2014-02-27 12:33:16 -0500

    Initialize repo, site is almost complete

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  
+  def contact
+  end
+
+  def environmental
+  end
+
+  def home
+  end
+
+  def webdev
+  end
+
+  def motion
+  end
+
+  def graphicdesign
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ConnorFinnerty::Application.config.secret_key_base = 'd02c977115bdc2813f858a47f413b0f75ac0bd7fd005721990913bb0fc5c990b6bc0da96299f551199b663e8bb4868560111c6c59ec0b1438a9141e3fd75e7c1'
